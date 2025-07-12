70b0a0a4b206467450133821683ac4906b7df7fa
martinsharma17 <martinsharma18@gmail.com>
2025-07-13 00:10:53 +0545

    home page

--- client/src/component/AiTools.jsx
+++ client/src/component/AiTools.jsx
@@ -1,6 +1,11 @@
+import { useUser } from '@clerk/clerk-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AiToolsData } from '../assets/assets'
 
 const AiTools = () => {
+    const navigate = useNavigate()
+    const { user } = useUser()
     return (
         <div className='px-4 sm:px-20 xl:px-32 my-24'>
             <div className='text-center'>
@@ -9,6 +14,21 @@ const AiTools = () => {
                 <p className='text-gray-500 max-w-1lg mx-auto'>Everything you need to
                     create, enhance, and optimize your content with cutting-edge AI technology.
                 </p>
+            </div>
+
+            <div className='flex flex-wrap mt-10 justify-center'>
+            {AiToolsData.map((tool,index) => (
+                <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE]
+shadow-1g border border-gray-100 hover:-translate-y-1 transition-all
+duration-3e0 cursor-pointer' onClick={()=> user && navigate(tool.path)} >
+<tool.Icon className='w-12 h-12 p-3 text-white rounded-x1'  style=
+{{background: `linear-gradient(to bottom, ${tool.bg.from}, ${tool.
+bg.to})` }}/>
+<h3 className='mt-6 mb-3 text-1lg font-semibold'>{tool.title}</h3>
+
+<p className='text-gray-400 text-sm max-w-[95%]'>{tool.description}</p>
+                </div>
+            ))}
             </div>
         </div>
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from '../component/Navbar'
 import Hero from '../component/Hero'
 import AiTools from '../component/AiTools'
+import Testimonial from '../component/Testimonial'
+import Plan from '../component/Plan'
 
 const Home = () => {
     return (
         <>
             <Navbar />
-            <Hero/>
-            <AiTools/>
+            <Hero />
+            <AiTools />
+            <Testimonial />
+            <Plan/>
         </>
     )
 }
