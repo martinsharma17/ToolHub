a1d90d3f64c4916823a87580321cc351bba4fe49
martinsharma17 <martinsharma18@gmail.com>
2025-07-16 00:23:26 +0545

    sidebar

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -1,23 +1,54 @@
-import { useClerk } from '@clerk/clerk-react';
-import React from 'react'
+import { useUser, useClerk } from '@clerk/clerk-react';
+import { House, SquarePen, Hash, Eraser, Scissors, FileText, Users } from 'lucide-react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+const navitems = [
+    { to: '/ai', label: 'Dashboard', Icon: House },
+    { to: '/ai/write-article', label: 'Write Article', Icon: SquarePen },
+    { to: '/ai/blog-titles', label: 'Blog Titles', Icon: Hash },
+    { to: '/ai/generate-images', label: 'Generate Images', Icon: Image },
+    { to: '/ai/remove-background', label: 'Remove Background', Icon: Eraser },
+    { to: '/ai/remove-object', label: 'Remove Object', Icon: Scissors },
+    { to: '/ai/review-resume', label: 'Review Resume', Icon: FileText },
+    { to: '/ai/community', label: 'Community', Icon: Users },
+]
 const Sidebar = ({ sidebar, setSidebar }) => {
     const { user } = useUser();
-    const { signOut, openUserProfile } = useClerk()
+    const { signOut, openUserProfile } = useClerk();
+
     return (
-        <div className={`w-60 bg-white border-r border-gray-200 flex flex-col
-justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar ?
-                'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300
-ease-in-out` }>
-
+        <aside
+            className={`w-60 bg-white border-r border-gray-200 flex flex-col
+      justify-between items-center max-sm:absolute top-14 bottom-0 
+      ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} 
+      transition-all duration-300 ease-in-out`}
+        >
             <div className='my-7 w-full'>
-                <img src={user.imhgeUrl} alt="User avatar" className='w-13 rounded-full
-mx-auto'/>
-                <hl className='mt-1 text-center'>{user.fullName}</hl>
+                <img
+                    src={user.imageUrl}
+                    alt="User avatar"
+                    className='w-13 rounded-full mx-auto'
+                />
+                <h1 className='mt-1 text-center'>{user.fullName}</h1>
+
+                <div>
+                    {navitems.map(({ to, label, Icon }) => {
+                        <NavLink key={to} to={to} end={to === '/ai'} onClick={() => setSidebar(false)} className={(isActive) => `px-3.5 py-2.5 flex items-center gap-3 rounded ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9234EA]' : ''}`}>
+
+                            {({ isActive }) => (
+                                <>
+                                    <Icon className={`w-4 h-4 ${isActive ? 'text-white' : ''} 
+`} />
+                                    {label}
+                                </>
+                            )}
+                        </NavLink>
+                    })}
+                </div>
             </div>
-        </div>
+        </aside>
+    );
+};
 
-    )
-}
-
-export default Sidebar
+export default Sidebar;
